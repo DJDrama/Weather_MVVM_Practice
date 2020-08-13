b0aa7d3e2050f5b52706d98057249395ac57ab6e
DJ <putzzang@gmail.com>
2020-08-14 03:06:15 +0900

    push test

--- app/src/main/java/com/dj/weather_mvvm/db/AppDatabase.kt
+++ app/src/main/java/com/dj/weather_mvvm/db/AppDatabase.kt
@@ -14,6 +14,7 @@ abstract class AppDatabase : RoomDatabase() {
 
     abstract fun weatherInfoDao(): WeatherInfoDao
     abstract fun dailyDao(): DailyDao
+    
 
     companion object {
         @Volatile
